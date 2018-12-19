186673e353a3501df39e2888a133e7cd142f373b
Toby Mackenzie <public@tobymackenzie.com>
2018-12-19 02:08:01 -0500

    add `find-files` command

---
+++ src/Command/FindFilesCommand.php
@@ -0,0 +1,59 @@
+<?php
+namespace TJM\TBin\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use TJM\Component\Console\Command\ContainerAwareCommand as Base;
+
+class FindFilesCommand extends Base{
+	static public $defaultName = 'find-files';
+	protected function configure(){
+		$this
+			->setDescription('Find files via the `find` command.  Optionally use `grep` command to find content.  Optionally do stuff with those files using run option.')
+			->addArgument('where', InputArgument::REQUIRED, 'SSH style host string of host to run command on.')
+			->addArgument('name', InputArgument::OPTIONAL, 'Look for files with name.')
+			->addOption('cd', 'd', InputOption::VALUE_REQUIRED, 'Directory to search at.')
+			->addOption('contents', 'c', InputOption::VALUE_REQUIRED, 'Search file contents for string.')
+			->addOption('exclude-paths', 'e', InputOption::VALUE_IS_ARRAY | InputOption::VALUE_REQUIRED, 'Path(s) to exclude from search.')
+			->addOption('find-options', 'o', InputOption::VALUE_REQUIRED, 'Options for the find command.')
+			->addOption('forward-agent', 'f', InputOption::VALUE_NONE, 'Forward local credentials for connecting to other servers from remote.')
+			->addOption('run', 'r', InputOption::VALUE_REQUIRED, 'Command to run on found files.')
+		;
+	}
+	protected function execute(InputInterface $input, OutputInterface $output){
+		//-!! logic should go into a service, but what service?  Shell? Files?
+		$opts = [];
+		if($input->getOption('cd')){
+			$opts['cd'] = $input->getOption('cd');
+		}
+		if($input->getOption('forward-agent')){
+			$opts['forwardAgent'] = true;
+		}
+		$command = "find .";
+		if($input->getOption('find-options')){
+			$command .= " {$input->getOption('find-options')}";
+		}
+		$excludePaths = $input->getOption('exclude-paths');
+		if($excludePaths){
+			foreach($excludePaths as $path){
+				$command .= " -not -path '{$path}'";
+			}
+		}
+		$name = $input->getArgument('name');
+		if($name){
+			$command .= " -name '{$name}'";
+		}
+		$run = $input->getOption('run');
+		$trailingCharacter = ($run ? ';' : '+');
+		if($input->getOption('contents')){
+			$grepOpts = ($run ? '-q' : '-l');
+			$command .= " -type f -exec grep {$grepOpts} '{$input->getOption('contents')}' {} \\{$trailingCharacter}";
+		}
+		if($run){
+			$command .= " -exec {$run} {} \\{$trailingCharacter}";
+		}
+		$output->writeln('Running: ' . $command);
+		$this->getContainer()->get('shell')->run($command ,$input->getArgument('where'), $opts);
+	}
+}
