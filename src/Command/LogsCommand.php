71353818e8621264cf397d21cc8f68f78ec4e7b1
Toby Mackenzie <public@tobymackenzie.com>
2021-03-18 15:36:51 -0400

    feat: add 'logs' command for reading log files

---
+++ src/Command/LogsCommand.php
@@ -0,0 +1,69 @@
+<?php
+namespace TJM\TBin\Command;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use TJM\ShellRunner\ShellRunner;
+
+class LogsCommand extends Command{
+	static public $defaultName = 'logs';
+	protected $shell;
+	public function __construct(ShellRunner $shell){
+		$this->shell = $shell;
+		parent::__construct();
+	}
+	protected function configure(){
+		$this
+			->setDescription('Read log files.')
+			->addArgument('name', InputArgument::REQUIRED, 'Look for log files with name.')
+			->addOption('contents', 'c', InputOption::VALUE_IS_ARRAY | InputOption::VALUE_REQUIRED, 'Find in log contents.')
+			->addOption('host', 'h', InputOption::VALUE_REQUIRED, 'SSH style host string of host to read log(s) on.')
+			->addOption('path', 'p', InputOption::VALUE_REQUIRED, 'Directory to find log in.', '/var/log')
+			->addOption('run', 'r', InputOption::VALUE_REQUIRED, 'Command to run on found log.', 'less')
+		;
+	}
+	protected function execute(InputInterface $input, OutputInterface $output){
+		//-!! logic should go into a service, but what service?  Shell? Files?
+		$opts = [];
+		$opts['path'] = $input->getOption('path');
+		$opts['command'] = "find . -type f";
+		//--name must work for find `-name` option, so we must move any dir to path
+		$pathBits = explode('/', $input->getArgument('name'));
+		$name = array_pop($pathBits);
+		$opts['path'] .= '/' . implode('/', $pathBits);
+		$opts['command'] .= " -name " . escapeshellarg($name);
+		$runOpts = explode(' ', trim($input->getOption('run')));
+		$run = array_shift($runOpts);
+		$contents = $input->getOption('contents');
+		$trailingCharacter = ($run === 'less' ? '+' : ';');
+
+		if($contents){
+			switch($run){
+				case 'less':
+					$runOpts[] = "--pattern=" . escapeshellarg(implode('|', $contents));
+				break;
+				case 'vi':
+				case 'vim':
+					$runOpts[] = "-c " . escapeshellarg('set hlsearch') . ' +/' . escapeshellarg(implode('|', $contents));
+				break;
+			}
+			$grepOpts = '-l';
+			$opts['command'] .= " -type f -exec grep {$grepOpts} " . escapeshellarg(array_shift($contents)) . " {} \\{$trailingCharacter}";
+			foreach($contents as $content){
+				$opts['command'] .= " | xargs grep {$grepOpts} " . escapeshellarg($content);
+			}
+		}
+		$opts['host'] = $input->getOption('host');
+		$opts['command'] .= " | xargs {$run} " . implode(' ', $runOpts);
+		$opts['interactive'] = true;
+		if($output->isVerbose()){
+			$output->writeln('Running: ' . $opts['command']);
+		}
+		$result = $this->shell->run($opts);
+		if(!$run){
+			$output->writeln($result);
+		}
+	}
+}
